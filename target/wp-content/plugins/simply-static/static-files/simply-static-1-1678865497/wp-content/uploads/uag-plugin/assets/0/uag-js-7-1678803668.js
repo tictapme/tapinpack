494453c47f3202c3e4bd852a400b3d826d31a8b6
GuilleF <guilleferrer@gmail.com>
2023-03-15 09:00:51 +0100

    Adds themes and plugins in repo

---
+++ target/wp-content/plugins/simply-static/static-files/simply-static-1-1678865497/wp-content/uploads/uag-plugin/assets/0/uag-js-7-1678803668.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){ 	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-055ce18f' );
+	});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"9a3bd5a3","reCaptchaEnable":true,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Env\u00edo del formulario","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Por favor, completa el captcha de arriba.","confirmationUrl":""}, '.uagb-block-9a3bd5a3', 7 );
+});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-11a6a1e0' );
+	});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"4bbedc63","reCaptchaEnable":true,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Env\u00edo del formulario","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Por favor, completa el captcha de arriba.","confirmationUrl":""}, '.uagb-block-4bbedc63', 7 );
+});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-2e042920' );
+	});
+window.addEventListener("DOMContentLoaded", function(){
+	UAGBForms.init( {"block_id":"cfe2ca61","reCaptchaEnable":true,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Env\u00edo del formulario","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Por favor, completa el captcha de arriba.","confirmationUrl":""}, '.uagb-block-cfe2ca61', 7 );
+});
+ })
