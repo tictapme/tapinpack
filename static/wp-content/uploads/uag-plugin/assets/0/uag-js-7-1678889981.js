1df2336fc93fee2f11655554e34a135a0e1a8fb9
GuilleF <guilleferrer@gmail.com>
2023-03-16 07:06:16 +0100

    Adds static files in repo

---
+++ static/wp-content/uploads/uag-plugin/assets/0/uag-js-7-1678889981.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ 	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-4bbdbd19' );
+	});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-cb7e1e95' );
+	});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-13a92d51' );
+	});
+ })
