d9af2dccf257fcf1ff9b590dd8db5a8609fbef4a
Javier Sampedro <jsampedro77@gmail.com>
2023-05-10 14:06:27 +0200

    update static

---
+++ static/wp-content/uploads/uag-plugin/assets/1000/uag-js-897-1683543194.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ 	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-74bb2d5a' );
+	});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-1bdac5f3' );
+	});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-ecaf923a' );
+	});
+ });
