290b71948811e0846044cbebb42f02d70788aa58
GuilleF <guilleferrer@gmail.com>
2023-06-09 11:14:06 +0200

    Updates static

---
+++ static/wp-content/uploads/uag-plugin/assets/0/uag-js-7-1686216103.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ 	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-ff156865' );
+	});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-0c7e4be5' );
+	});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-4c77abbb' );
+	});
+ });
---
+++ static/wp-content/uploads/uag-plugin/assets/1000/uag-js-897-1686216106.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ 	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-97c0742a' );
+	});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-b30f20bc' );
+	});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-4480ac20' );
+	});
+ });
