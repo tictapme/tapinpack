71aef4ce1245522951d88b3f15bdda8233f477be
GuilleF <guilleferrer@gmail.com>
2023-11-29 12:37:25 +0100

    Updates static

---
+++ static/wp-content/uploads/uag-plugin/assets/0/uag-js-7-1701257502.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){ 	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-8085de77' );
+	});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-03b80e0b' );
+	});
+	window.addEventListener( 'DOMContentLoaded', function() {
+		UAGBModal.init( '.uagb-block-7cbbb46b' );
+	});
+ });
